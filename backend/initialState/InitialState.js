c9d93c82e248994f8e197c3c8645537a4eaaf475
your_name <your_email_id>
2021-11-13 08:16:59 +0500

    push to github

--- backend/initialState/InitialState.js
+++ backend/initialState/InitialState.js
@@ -1,14 +1,74 @@
-export const initialState = [
-    {
-        id:0,
-        name:'Admin',
-        email:'admin@gmail.com',
-        password: 1234,
-    },
-    {
-        id:1,
-        name:'User',
-        email:'user@gmail.com',
-        password: 5678,
-    },
-];
+export const initialState = {
+    users: [
+        {
+            id:0,
+            name:'Admin',
+            email:'admin@gmail.com',
+            password: 1234,
+            describtion:'it is good high quality product!',
+        },
+        {
+            id:1,
+            name:'User',
+            email:'user@gmail.com',
+            password: 5678,
+            describtion:'it is bad really I don`t interested this one!!!',
+        },
+    ],
+    products: [
+        {
+            id:1,
+            image:"./images/p1.jpg",
+            name: 'Asus',
+            price: '$220',
+        },
+        {
+            id:2,
+            image:"./images/p2.jpg",
+            name: 'Olg',
+            price: '$180',
+        },
+        {
+            id:3,
+            image:"./images/p3.jpg",
+            name: 'Artel',
+            price: '$160',
+        },
+        {
+            id:4,
+            image:"./images/p4.jpg",
+            name: 'Lenovo',
+            price: '$150',
+        },
+        {
+            id:5,
+            image:"./images/p5.jpg",
+            name: 'Mac',
+            price: '$140',
+        },
+        {
+            id:6,
+            image:"./images/p6.jpg",
+            name: 'Think',
+            price: '$100',
+        },
+        {
+            id:7,
+            image:"./images/p7.jpg",
+            name: 'HP',
+            price: '$250',
+        },
+        {
+            id:8,
+            image:"./images/p8.jpg",
+            name: 'Apple',
+            price: '$50',
+        },
+        {
+            id:9,
+            image:"./images/p9.jpg",
+            name: 'Acer',
+            price: '$130',
+        },
+    ]
+}
